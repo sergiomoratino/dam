5794c7e2043e57e4cc2675f29356a5cd50018862
Sergio <sergio.moratino@gmail.com>
2020-02-10 16:54:03 +0100

    class-instanced

---
+++ clientes.py
@@ -0,0 +1,19 @@
+import conexion
+
+class clientes:
+    dni = ''    
+    def consultas():
+        cursor = conexion.conecta()        
+        sql_query = "SELECT DNI from Clientes";       
+        try :
+            cursor.execute( sql_query );
+            data = cursor.fetchone();
+            print( "Database Version: %s" %data );
+        
+        except Exception as e :
+            print("Exception : ", e);
+
+
+
+
+        
--- erp.py
+++ erp.py
@@ -1,20 +1,9 @@
 #!/usr/bin/python3
 import conexion
-import Clientes
+from clientes import clientes
 
-def consultas():
-    cursor = conexion.conecta()
-    
-    sql_query = "SELECT Nombre from Clientes";
-    
-    try :
-        cursor.execute( sql_query );
-        data = cursor.fetchone();
-        print( "Database Version: %s" %data );
-       
-    except Exception as e :
-        print("Exception : ", e);
+
         
 
-#consultas()
-Clientes.getDNI()
+clientes.consultas()
+
