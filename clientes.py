0ad1b22e95df9ed02f6aff71aba9be655c4da923
sofianN-J <sofiannaimi96@gmail.com>
2020-02-10 17:06:43 +0100

    insert-first-getters

--- clientes.py
+++ clientes.py
@@ -13,8 +13,37 @@ class clientes:
         except Exception as e :
             print("Exception : ", e);
 
-dsafdf
-    
+    def getNombre()
+        cursor = conexion.conecta()        
+        sql_query = "SELECT nombre from Clientes";       
+        try :
+            cursor.execute( sql_query );
+            data = cursor.fetchone();
+            print( "Database Version: %s" %data );
+        
+        except Exception as e :
+            print("Exception : ", e);
 
+    def getApellidos()
+        cursor = conexion.conecta()        
+        sql_query = "SELECT apellidos from Clientes";       
+        try :
+            cursor.execute( sql_query );
+            data = cursor.fetchone();
+            print( "Database Version: %s" %data );
+        
+        except Exception as e :
+            print("Exception : ", e);
+            
+    def getGenero()
+        cursor = conexion.conecta()        
+        sql_query = "SELECT apellidos from Clientes";       
+        try :
+            cursor.execute( sql_query );
+            data = cursor.fetchone();
+            print( "Database Version: %s" %data );
+        
+        except Exception as e :
+            print("Exception : ", e);
 
         
